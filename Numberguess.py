8be7e3a246b43517028cff6c73414eb7626aca64
Austinr-12 <193960113+Austinr-12@users.noreply.github.com>
2026-08-31 12:58:19 -0500

    main guessing loop finished

--- Numberguess.py
+++ Numberguess.py
@@ -14,3 +14,22 @@ ch = 7
 
 # Guess Counter
 gc = 0
+
+while gc < ch:
+    gc += 1 
+    guess = int(input('Enter your guess: '))
+
+    if guess == num:
+        print(f'Correct! The number is {num} You guessed it in {gc} attempts.')
+        break
+
+    elif gc >= ch and guess != num:
+        print(f'Sorry! that number was {num} Better luck next time.')
+
+    elif guess > num:
+        print('Too High! Try a lower number.')
+
+    elif guess < num:
+        print('Too low! Try a higher number.')
+
+        
