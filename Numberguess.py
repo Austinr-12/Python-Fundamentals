6dfd94f6246dc3b9f9b0edbc731b1a23e74e76d9
Austinr-12 <193960113+Austinr-12@users.noreply.github.com>
2026-08-31 12:45:22 -0500

    setup and intro created

--- Numberguess.py
+++ Numberguess.py
@@ -1,3 +1,16 @@
 import random
 
 print("Hello!, Welcome to the Number Guessing Game. \n You have 7 chances to guess the number. Let's start!")
+
+low = int(input("Enter the Lower Bound: "))
+high = int(input("Enter the upper Bound"))
+
+print(f"\nYou have 7 chances to guess the number between {low} and {high}")
+
+num = random.randint(low, high)
+
+# Total allowed chances
+ch = 7
+
+# Guess Counter
+gc = 0
