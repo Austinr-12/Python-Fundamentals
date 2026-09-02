997543e09f3e60c46d70655e97c13bcfc8573382
Austinr-12 <austin.ramirez1@outlook.com>
2026-09-02 17:36:17 -0500

    logic for game loop

--- WordGuess.py
+++ WordGuess.py
@@ -15,3 +15,15 @@ guesses = ''
 
 turns = 12
 
+#beginning main loop for game
+while turns > 0:
+    failedd = 0
+
+    for char in word:
+        if char in guesses:
+            print(char, end = " ")
+        else:
+            print("_", end = " ")
+            failed += 1
+
+    
