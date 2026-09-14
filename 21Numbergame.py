990ee75baf0eecf5951c87bff0d4b00314cb6844
Austinr-12 <austin.ramirez1@outlook.com>
2026-09-14 12:44:22 -0500

    setting up functions for later and new number game

---
+++ 21Numbergame.py
@@ -0,0 +1,19 @@
+def nearestMultiple(num):
+    if num >= 4:
+        near = num + (4 - (num % 4))
+    else:
+        near = 4
+    return near
+
+def lose1():
+    print("\n\nYOU LOSE!")
+    print("Better luck next time!")
+    exit(0)
+
+def check(xyz):
+    i = 1
+    while i < len(xyz):
+        if (xyz[i] - xyz[i - 1]) != 1:
+            return False
+        i += 1
+    return True
