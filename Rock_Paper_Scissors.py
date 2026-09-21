3299b8a4dd9a3d5e1b4bc047521c54059d23054e
Austinr-12 <austin.ramirez1@outlook.com>
2026-09-21 17:26:03 -0500

    validating input

--- Rock_Paper_Scissors.py
+++ Rock_Paper_Scissors.py
@@ -8,3 +8,17 @@ print("Rock vs Scissors -> Rock wins")
 print("Paper vs Scissors -> Scissors wins\n")
 
 choices = ["Rock", "Paper", "Scissors"]
+
+while True:
+
+    print("Choose an option:")
+    print("1 - Rock")
+    print("2 - Paper")
+    print("3 - Scissors")
+
+    #Validate user input
+    try:
+        choice = int(input("Enter your choice: "))
+    except ValueError:
+        print("Please enter a valid number.\n")
+        continue
