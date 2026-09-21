7a3b717f2fb37320191a88600f50e0a59ebb71e2
Austinr-12 <austin.ramirez1@outlook.com>
2026-09-21 17:23:19 -0500

    game rules and setting up choices

--- Rock_Paper_Scissors.py
+++ Rock_Paper_Scissors.py
@@ -1,1 +1,10 @@
 import random
+
+# Game Rules
+print("Welcome to Rock-Paper-Scissors!\n")
+print("Winning Rules:")
+print("Rock vs Paper -> Paper wins")
+print("Rock vs Scissors -> Rock wins")
+print("Paper vs Scissors -> Scissors wins\n")
+
+choices = ["Rock", "Paper", "Scissors"]
